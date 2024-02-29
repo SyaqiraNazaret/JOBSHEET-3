dcc47b1801de23912d855cd2138e3bff66b191e7
SyaqiraNazaret <160091015+SyaqiraNazaret@users.noreply.github.com>
2024-02-29 09:58:22 +0700

    Add files via upload

--- Pertemuan 03/PersegiPanjangDemo.java
+++ Pertemuan 03/PersegiPanjangDemo.java
@@ -1,25 +1,37 @@
-/**
- * PersegiPanjangDemo
- */
+import java.util.Scanner;
 public class PersegiPanjangDemo {
 
 public static void main(String[] args){
-    PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
+    Scanner sc = new Scanner(System.in);
+     PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
 
-    arrayOfPersegiPanjang[0] = new PersegiPanjang();
-    arrayOfPersegiPanjang[0].panjang = 110;
-    arrayOfPersegiPanjang[0].lebar = 30;
+     for (int i = 0; i< 3; i++){
+        arrayOfPersegiPanjang[i] = new PersegiPanjang();
 
-    arrayOfPersegiPanjang[1] = new PersegiPanjang();
-    arrayOfPersegiPanjang[1].panjang = 80;
-    arrayOfPersegiPanjang[1].lebar = 40;
+        System.out.println("Persegi Panjang ke-" + (i+1));
+        System.out.print("Masukkan Panjang: ");
+        arrayOfPersegiPanjang[i].panjang = sc.nextInt();
+        System.out.print("Masukkan Lebar: ");
+        arrayOfPersegiPanjang[i].lebar = sc.nextInt();
+    }
+    for  (int j = 0; j < 3; j++) {
+        System.out.println("Persegi Panjang ke-" + (j+1)+ ", panjang: " + arrayOfPersegiPanjang[j].panjang + ", lebar: " + arrayOfPersegiPanjang[j].lebar);
+    }
 
-    arrayOfPersegiPanjang[2] = new PersegiPanjang();
-    arrayOfPersegiPanjang[2].panjang = 100;
-    arrayOfPersegiPanjang[2].lebar = 20;
+    // arrayOfPersegiPanjang[0] = new PersegiPanjang();
+    // arrayOfPersegiPanjang[0].panjang = 110;
+    // arrayOfPersegiPanjang[0].lebar = 30;
 
-    System.out.println("Persegi Panjang ke-1, panjang: " + arrayOfPersegiPanjang[0].panjang+ ", lebar " + arrayOfPersegiPanjang[0].lebar);
-    System.out.println("Persegi Panjang ke-2, panjang: " + arrayOfPersegiPanjang[1].panjang+ ", lebar " + arrayOfPersegiPanjang[1].lebar);
-    System.out.println("Persegi Panjang ke-3, panjang: " + arrayOfPersegiPanjang[2].panjang+ ", lebar " + arrayOfPersegiPanjang[2].lebar);
+    // arrayOfPersegiPanjang[1] = new PersegiPanjang();
+    // arrayOfPersegiPanjang[1].panjang = 80;
+    // arrayOfPersegiPanjang[1].lebar = 40;
+
+    // arrayOfPersegiPanjang[2] = new PersegiPanjang();
+    // arrayOfPersegiPanjang[2].panjang = 100;
+    // arrayOfPersegiPanjang[2].lebar = 20;
+
+    // System.out.println("Persegi Panjang ke-1, panjang: " + arrayOfPersegiPanjang[0].panjang+ ", lebar " + arrayOfPersegiPanjang[0].lebar);
+    // System.out.println("Persegi Panjang ke-2, panjang: " + arrayOfPersegiPanjang[1].panjang+ ", lebar " + arrayOfPersegiPanjang[1].lebar);
+    // System.out.println("Persegi Panjang ke-3, panjang: " + arrayOfPersegiPanjang[2].panjang+ ", lebar " + arrayOfPersegiPanjang[2].lebar);
 }
 }
